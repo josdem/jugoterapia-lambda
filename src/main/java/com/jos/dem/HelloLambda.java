5030502336323bbc3d936c25319534888f5e4621
josdem <joseluis.delacruz@gmail.com>
2016-12-14 17:09:48 -0600

    Adding model to the lambda service

--- src/main/java/com/jos/dem/HelloLambda.java
+++ src/main/java/com/jos/dem/HelloLambda.java
@@ -2,7 +2,7 @@ package com.jos.dem;
 
 import com.amazonaws.services.lambda.runtime.Context;
 
-public class Hello {
+public class HelloLambda {
 
   public String mainHandler(String name, Context context) {
     return String.format("Hello %s.", name);
---
+++ src/main/java/com/jos/dem/model/Person.java
@@ -0,0 +1,25 @@
+package com.jos.dem.model;
+
+public class Person {
+  private String name;
+  private String lastname;
+
+  public void setName(String name){
+    this.name = name;
+  }
+
+  public String getName(){
+    return name;
+  }
+
+  public void setLastname(String lastname){
+    this.lastname = lastname;
+  }
+
+  public String getLastname(){
+    return lastname;
+  }
+
+}
+
+
